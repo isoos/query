4ea23ef75297a5b113ac2e39f2d1c061bc728599
danny <dtylman@gmail.com>
2021-10-10 12:55:44 +0300

    upgrading to petitparser: ^4.3.0

--- lib/query.dart
+++ lib/query.dart
@@ -1,9 +1,10 @@
+import 'package:query/src/grammar.dart';
+
 import 'src/ast.dart';
-import 'src/grammar.dart';
 
 export 'src/ast.dart';
 
-final _parser = QueryParser();
+final _parser = QueryGrammarDefinition().build();
 
 /// Parses [input] and returns a parsed [Query].
 Query parseQuery(String input) {
